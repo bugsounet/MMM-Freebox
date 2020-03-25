a818b51069f1fe29af1447c1a39f6df730614509
bugsounet <github@bugsnana.fr>
2020-03-25 13:14:24 +0100

    update

--- Freebox_Login.js
+++ Freebox_Login.js
@@ -2,8 +2,8 @@ const { FreeboxRegister } = require("freebox");
 
 async function main() {
   const freeboxRegister = new FreeboxRegister({
-    app_id: "fbx.HomeStatus",
-    app_name: "HomeStatus",
+    app_id: "fbx.MMM-Freebox",
+    app_name: "Freebox",
     app_version: "1.0.0",
     device_name: "Magic Mirror",
   });
@@ -14,40 +14,3 @@ async function main() {
 }
 
 main().catch(err => console.error(err));
-
-/*
-
-Please check your Freebox Server LCD screen and authorize application access to register your app.
-
-Your app has been granted access !
-
-Save safely those following informations secret to connect to your Freebox API:
-{ app_token:
-   'etCEF2aytGPLWm1KZM0vIW/ziZOU58v/0qv9jUiJcedjadjaRZ/bflWSKy6HODORGUo6',
-  app_id: 'fbx.HomeStatus',
-  api_domain: 'r42bhm9p.fbxos.fr',
-  https_port: 35023,
-  api_base_url: '/api/',
-  api_version: '6.0' }
-
-->>>
-
-and report this information in Freebox module rate section:
-
-for example
-
-Freebox: {
-			active: false,
-			player_ip: "192.168.0.250",
-			server_ip: "192.168.0.254",
-			rate : {
-				active : true,
-				app_token: "etCEF2aytGPLWm1KZM0vIW/ziZOU58v/0qv9jUiJcedjadjaRZ/bflWSKy6HODORGUo6",
-				app_id: "bx.HomeStatus",
-				api_domain: "r42bhm9p.fbxos.fr",
-				https_port: 35023
-			}
-		},
-
-
-*/
