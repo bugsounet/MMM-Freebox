31d26bd11bc31efc510916a2f7ae5905a38b9651
bugsounet <github@bugsounet.fr>
2024-03-29 13:52:55 +0100

    lint node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -1,154 +1,154 @@
-var NodeHelper = require('node_helper')
-const { Freebox } = require("./components/freebox.js")
-var ping = require('ping')
+var NodeHelper = require("node_helper");
+var ping = require("ping");
+const { Freebox } = require("./components/freebox.js");
 
-FB = (...args) => { /* do nothing */ }
+FB = (...args) => { /* do nothing */ };
 
 module.exports = NodeHelper.create({
-  start: function() {
-    console.log("[Freebox] Starting...")
-    this.freebox = null
-    this.pingValue = null
-    this.interval = null
-    this.cache = {}
-    this.update = null
-    this.FreeboxVersion = null
-    this.init = false
+  start () {
+    console.log("[Freebox] Starting...");
+    this.freebox = null;
+    this.pingValue = null;
+    this.interval = null;
+    this.cache = {};
+    this.update = null;
+    this.FreeboxVersion = null;
+    this.init = false;
   },
 
-  Freebox: async function (config) {
+  async Freebox (config) {
     this.Freebox_OS(config,this.config.showClientRate || this.config.showClientCnxType).then(
       (res) => {
-        if (Object.keys(this.cache).length == 0) this.makeCache(res)
+        if (Object.keys(this.cache).length === 0) this.makeCache(res);
         else {
-          this.makeResult(res)
-          this.updateInterval()
+          this.makeResult(res);
+          this.updateInterval();
         }
       },
       (err) => {
-        console.error("[Freebox] " + err)
-        this.updateInterval()
+        console.error(`[Freebox] ${  err}`);
+        this.updateInterval();
       }
-    )
+    );
   },
 
-  scan: function() {
-    if (this.config.showPing) this.Ping()
-    this.Freebox(this.config.freebox)
+  scan () {
+    if (this.config.showPing) this.Ping();
+    this.Freebox(this.config.freebox);
   },
 
   /** scan main loop **/
-  updateInterval: function () {
-    clearInterval(this.update)
-    this.counterUpdate = this.config.updateDelay
+  updateInterval () {
+    clearInterval(this.update);
+    this.counterUpdate = this.config.updateDelay;
 
     this.update = setInterval( ()=> {
-      this.counterUpdate -= 1000
+      this.counterUpdate -= 1000;
       if (this.counterUpdate <= 0) {
-        clearInterval(this.update)
-        this.scan()
+        clearInterval(this.update);
+        this.scan();
       }
-    }, 1000)
+    }, 1000);
   },
 
-  socketNotificationReceived: function(notification, payload) {
+  socketNotificationReceived (notification, payload) {
     switch(notification) {
       case "INIT":
-        this.config = payload
-        if (this.config.debug) FB = (...args) => { console.log("[Freebox]", ...args) }
+        this.config = payload;
+        if (this.config.debug) FB = (...args) => { console.log("[Freebox]", ...args); };
         console.log("[Freebox] Version:", require("./package.json").version, "rev:", require("./package.json").rev);
-        this.scan()
+        this.scan();
         console.log("[Freebox] Started!");
-        break
+        break;
       case "SCAN":
-        this.updateInterval()
-        break
+        this.updateInterval();
+        break;
       case "CACHE":
-        this.cache = {}
-        this.scan()
-        this.updateInterval()
-        break
+        this.cache = {};
+        this.scan();
+        this.updateInterval();
+        break;
     }
   },
 
-  sendInfo: function (noti, payload) {
-    if (this.config.verbose) FB(`Send notification: ${noti}`, payload)
-    this.sendSocketNotification(noti, payload)
+  sendInfo (noti, payload) {
+    if (this.config.verbose) FB(`Send notification: ${noti}`, payload);
+    this.sendSocketNotification(noti, payload);
   },
 
-  makeCache: function (res) {
-    this.cache = {}
-    if (this.config.debug )this.sendSocketNotification("debug", res)
+  makeCache (res) {
+    this.cache = {};
+    if (this.config.debug )this.sendSocketNotification("debug", res);
     if (Object.keys(res.Client).length > 0) {
       for (let [item, client] of Object.entries(res.Client)) {
         this.cache[client.l2ident.id] = {
           name: client.primary_name ? client.primary_name : "(Appareil sans nom)",
           type: client.host_type,
-          show: (!this.config.showFree && client.vendor_name.toLowerCase() == "freebox sas") ? false : this.config.showClient
-        }
+          show: (!this.config.showFree && client.vendor_name.toLowerCase() === "freebox sas") ? false : this.config.showClient
+        };
       }
     }
 
-    this.cache = this.sortBy(this.cache, this.config.sortBy)
+    this.cache = this.sortBy(this.cache, this.config.sortBy);
 
-    this.sendInfo("INITIALIZED", this.cache)
-    this.makeResult(res)
+    this.sendInfo("INITIALIZED", this.cache);
+    this.makeResult(res);
   },
 
-  sortBy: function (data, sort) {
-    var result = {}
+  sortBy (data, sort) {
+    var result = {};
     /** sort by type or by name **/
-    if (sort == "type" || sort == "name") {
-      FB("Sort cache by" , sort)
-      var arr = []
+    if (sort === "type" || sort === "name") {
+      FB("Sort cache by" , sort);
+      var arr = [];
       for (var mac in data) {
         if (data.hasOwnProperty(mac)) {
-            var obj = {}
-            obj[mac] = data[mac]
-            obj.Sort = data[mac][sort].toLowerCase()
-            arr.push(obj)
+          var obj = {};
+          obj[mac] = data[mac];
+          obj.Sort = data[mac][sort].toLowerCase();
+          arr.push(obj);
         }
       }
 
       arr.sort((a, b)=> {
-        var at = a.Sort
-        var bt = b.Sort
-        return at > bt ? 1 : ( at < bt ? -1 : 0 )
-      })
+        var at = a.Sort;
+        var bt = b.Sort;
+        return at > bt ? 1 : ( at < bt ? -1 : 0 );
+      });
 
       for (var i=0, l=arr.length; i<l; i++) {
         var obj = arr[i];
-        delete obj.Sort
+        delete obj.Sort;
         for (var mac in obj) {
           if (obj.hasOwnProperty(mac)) {
-              var id = mac
+            var id = mac;
           }
         }
 
-        result[mac] = obj[id]
+        result[mac] = obj[id];
       }
-    } else if (sort == "mac") {
+    } else if (sort === "mac") {
       /** sort by MAC **/
-      FB("Sort cache by", sort)
-      var mac = Object.keys(data)
-      mac.sort()
+      FB("Sort cache by", sort);
+      var mac = Object.keys(data);
+      mac.sort();
       mac.forEach((macSort)=> {
-        result[macSort] = data[macSort]
-      })
+        result[macSort] = data[macSort];
+      });
     } else {
       /** other return the same **/
-      FB("Cache not sorted")
-      result = data
+      FB("Cache not sorted");
+      result = data;
     }
-    return result
+    return result;
   },
 
-  makeResult: function(res) {
-    res.Clients = []
+  makeResult (res) {
+    res.Clients = [];
 
-    var device = {}
+    var device = {};
     
-    res.Client.forEach(client=> {
+    res.Client.forEach((client)=> {
     /** Array of client with used value in object **/
       device = {
         mac: client.l2ident.id,
@@ -164,163 +164,162 @@ module.exports = NodeHelper.create({
         signal_percent: null,
         signal_bar: null,
         eth: null
-      }
+      };
       
-      let ip = client?.l3connectivities.find(cnx => cnx.af === "ipv4" && cnx.active)
-      device.ip = ip ? ip.addr : null
+      let ip = client?.l3connectivities.find((cnx) => cnx.af === "ipv4" && cnx.active);
+      device.ip = ip ? ip.addr : null;
       if (client.access_point?.connectivity_type === "wifi") {
-        if (client.access_point?.wifi_information.band === "2d4g") device.access_type= "wifi2"
-        if (client.access_point?.wifi_information.band === "5g") device.access_type= "wifi5"
-        if (client.access_point?.wifi_information.band === "6g") device.access_type= "wifi6"
-        if (client.access_point?.wifi_information.band === "60g") device.access_type= "wifi7"
+        if (client.access_point?.wifi_information.band === "2d4g") device.access_type= "wifi2";
+        if (client.access_point?.wifi_information.band === "5g") device.access_type= "wifi5";
+        if (client.access_point?.wifi_information.band === "6g") device.access_type= "wifi6";
+        if (client.access_point?.wifi_information.band === "60g") device.access_type= "wifi7";
         if (client.access_point?.wifi_information.signal) {
-          device.signal = client.access_point.wifi_information.signal
-          device.signal_percent = this.wifiPercent(device.signal)
-          device.signal_bar = this.wifiBar(device.signal_percent)
+          device.signal = client.access_point.wifi_information.signal;
+          device.signal_percent = this.wifiPercent(device.signal);
+          device.signal_bar = this.wifiBar(device.signal_percent);
         }
-        if (client.access_point?.tx_rate) device.debit = this.convert(client.access_point.tx_rate*8,0,1) // Warn debit en bytes! (base 8)
-        else device.debit = "0 Ko/s"
+        if (client.access_point?.tx_rate) device.debit = this.convert(client.access_point.tx_rate*8,0,1); // Warn debit en bytes! (base 8)
+        else device.debit = "0 Ko/s";
       }
 
       if (this.config.showClientRate || this.config.showClientCnxType) {
-        res.EthCnx.forEach(info=> {
+        res.EthCnx.forEach((info)=> {
           if (info.mac_list) {
-            if (!info.mac_list.length) return // return an object ???
-            var macList = info.mac_list.map((mac_list)=>{return mac_list.mac})
-            macList.forEach(mac => {
-              if (client.l2ident.id == mac) {
-                if (client.access_point?.connectivity_type === "wifi" && client.access_point?.type === "repeater") device.repeater = true
+            if (!info.mac_list.length) return; // return an object ???
+            var macList = info.mac_list.map((mac_list)=>{return mac_list.mac;});
+            macList.forEach((mac) => {
+              if (client.l2ident.id === mac) {
+                if (client.access_point?.connectivity_type === "wifi" && client.access_point?.type === "repeater") device.repeater = true;
                 else if (res[info.id] && res[info.id].tx_bytes_rate) {
-                  if (this.config.showEthClientRate) device.debit = this.convert(res[info.id].tx_bytes_rate,0,1)
-                  device.access_type = "ethernet"
-                  device.eth = info.id
+                  if (this.config.showEthClientRate) device.debit = this.convert(res[info.id].tx_bytes_rate,0,1);
+                  device.access_type = "ethernet";
+                  device.eth = info.id;
                 }
                 else {
                   /* try again next time */
                 }
               }
-            })
+            });
           }
-        })
+        });
       }
 
       // consider virtual machine is connected without access point and with only 1 names length !
       // -> with only 1 names length -> prevent fake VM detect on disconnect (don't find other solution...)
-      if (!client.access_point && client.active && client.names.length === 1) device.access_type = "VM"
-      res.Clients.push(device)
-    })
+      if (!client.access_point && client.active && client.names.length === 1) device.access_type = "VM";
+      res.Clients.push(device);
+    });
 
     /** delete all Freebox result **/
-    delete res.Client
-    delete res[1]
-    delete res[2]
-    delete res[3]
-    delete res[4]
-    delete res[5]
-    delete res[6]
-    delete res.EthCnx
+    delete res.Client;
+    delete res[1];
+    delete res[2];
+    delete res[3];
+    delete res[4];
+    delete res[5];
+    delete res[6];
+    delete res.EthCnx;
 
-    res.Ping = this.config.showPing ? this.pingValue : null
-    this.sendInfo("RESULT", res)
+    res.Ping = this.config.showPing ? this.pingValue : null;
+    this.sendInfo("RESULT", res);
   },
 
-  Ping: function() {
+  Ping () {
     ping.promise.probe(this.config.pingAdress,
       {
         timeout: 2,
-        extra: ['-4']
-      }
-    )
-    .then((res)=> {
-      if (res.alive) this.pingValue = res.time + " ms"
-      else this.pingValue = "Erreur !"
-    })
+        extra: ["-4"]
+      })
+      .then((res)=> {
+        if (res.alive) this.pingValue = `${res.time} ms`;
+        else this.pingValue = "Erreur !";
+      });
   },
 
-/** Freebox OS API CALL **/
-  Freebox_OS: async function(config,clientRate) {
-    FB("Start Query Freebox Server:")
-    var rate
-    var output
+  /** Freebox OS API CALL **/
+  async Freebox_OS (config,clientRate) {
+    FB("Start Query Freebox Server:");
+    var rate;
+    var output;
 
-    const freebox = new Freebox(config)
-    await freebox.login()
+    const freebox = new Freebox(config);
+    await freebox.login();
 
     if (!this.init) {
-      FB("Quering Freebox Model...")
+      FB("Quering Freebox Model...");
       const FreeboxVersion = await freebox.request({
         method: "GET",
         url: "api_version/"
-      })
+      });
 
-      this.FreeboxVersion = this.ModelSearh(FreeboxVersion.data.box_model_name)
-      FB("Found:", this.FreeboxVersion)
-      this.init = true
+      this.FreeboxVersion = this.ModelSearh(FreeboxVersion.data.box_model_name);
+      FB("Found:", this.FreeboxVersion);
+      this.init = true;
     }
 
-    FB("Quering Client...")
+    FB("Quering Client...");
     const clients = await freebox.request({
       method: "GET",
       url: "lan/browser/pub/"
-    })
+    });
 
-    FB("Quering ALL Ethernet Cnx...")
-      var ethCnx = await freebox.request({
-        method: "GET",
-        url:"switch/status/"
-      })
+    FB("Quering ALL Ethernet Cnx...");
+    var ethCnx = await freebox.request({
+      method: "GET",
+      url:"switch/status/"
+    });
 
     if (clientRate) {
-      FB("Quering Ethernet on port 1...")
+      FB("Quering Ethernet on port 1...");
       var eth1 = await freebox.request({
         method: "GET",
         url:"switch/port/1/stats"
-      })
+      });
 
-      FB("Quering Ethernet on port 2...")
+      FB("Quering Ethernet on port 2...");
       var eth2 = await freebox.request({
         method: "GET",
         url:"switch/port/2/stats"
-      })
+      });
 
-      FB("Quering Ethernet on port 3...")
+      FB("Quering Ethernet on port 3...");
       var eth3 = await freebox.request({
         method: "GET",
         url:"switch/port/3/stats"
-      })
+      });
 
-      FB("Quering Ethernet on port 4...")
+      FB("Quering Ethernet on port 4...");
       var eth4 = await freebox.request({
         method: "GET",
         url:"switch/port/4/stats"
-      })
+      });
 
       if (this.config.checkFreePlug) {
-        FB("Quering Freeplug...")
+        FB("Quering Freeplug...");
         var eth5 = await freebox.request({
           method: "GET",
           url:"switch/port/5/stats"
-        })
+        });
       }
       if (this.config.checkSFP) {
-        FB("Quering SFP...")
+        FB("Quering SFP...");
         var eth6 = await freebox.request({
           method: "GET",
           url:"switch/port/9999/stats"
-        })
+        });
       }
     }
-    FB("Quering Connexion...")
+    FB("Quering Connexion...");
     const cnx = await freebox.request({
       method: "GET",
       url: "connection/"
-    })
-    FB("Done!")
+    });
+    FB("Done!");
 
-    bandwidth = this.convert(cnx.data.result.bandwidth_down,1,2) + " - " + this.convert(cnx.data.result.bandwidth_up,1,2)
-    debit = this.convert(cnx.data.result.rate_down,0,2) +" - " + this.convert(cnx.data.result.rate_up,0,2)
-    type = (cnx.data.result.media == "xdsl") ? "xDSL" : ((cnx.data.result.media == "ftth") ? "FTTH" : "Inconnu")
-    degroup = (cnx.data.result.type == "rfc2684") ? true : false
+    bandwidth = `${this.convert(cnx.data.result.bandwidth_down,1,2)} - ${this.convert(cnx.data.result.bandwidth_up,1,2)}`;
+    debit = `${this.convert(cnx.data.result.rate_down,0,2)} - ${this.convert(cnx.data.result.rate_up,0,2)}`;
+    type = (cnx.data.result.media === "xdsl") ? "xDSL" : ((cnx.data.result.media === "ftth") ? "FTTH" : "Inconnu");
+    degroup = (cnx.data.result.type === "rfc2684") ? true : false;
 
     output = {
       Model: this.FreeboxVersion,
@@ -336,56 +335,56 @@ module.exports = NodeHelper.create({
       3: clientRate && eth3.data.result ? eth3.data.result : [],
       4: clientRate && eth4.data.result ? eth4.data.result : [],
       5: clientRate && this.config.checkFreePlug && eth5.data.result ? eth5.data.result : [],
-      6: clientRate && this.config.checkSFP && eth6.data.result ? eth6.data.result : [],
-    }
+      6: clientRate && this.config.checkSFP && eth6.data.result ? eth6.data.result : []
+    };
 
-    await freebox.logout()
-    return output
+    await freebox.logout();
+    return output;
   },
   
-  convert: function(data,type=0,FixTo=0) {
+  convert (data,type=0,FixTo=0) {
     // type 0: octet / type:1 bytes
     var value = Number(data);
 
     if (value>1000000000) {
-      value=Number((value/1000000000).toFixed(FixTo))
-      if (type === 0) value = value + " Go/s"
-      if (type === 1) value = value + " Gb/s"
-      return value
+      value=Number((value/1000000000).toFixed(FixTo));
+      if (type === 0) value = `${value  } Go/s`;
+      if (type === 1) value = `${value  } Gb/s`;
+      return value;
     }
     if (value>1000000) {
-      value=Number((value/1000000).toFixed(FixTo))
-      if (type === 0) value = value + " Mo/s"
-      if (type === 1) value = value + " Mb/s"
-      return value
+      value=Number((value/1000000).toFixed(FixTo));
+      if (type === 0) value = `${value  } Mo/s`;
+      if (type === 1) value = `${value  } Mb/s`;
+      return value;
     }
-    value=Number((value/1000).toFixed(FixTo))
-    if (type === 0) value = value + " Ko/s"
-    if (type === 1) value = value + " Kb/s"
-    return value
+    value=Number((value/1000).toFixed(FixTo));
+    if (type === 0) value = `${value  } Ko/s`;
+    if (type === 1) value = `${value  } Kb/s`;
+    return value;
   },
 
   /** Signal wifi en % **/
-  wifiPercent(dB) {
+  wifiPercent (dB) {
     if(dB <= -100)
       quality = 0;
     else if(dB >= -50)
       quality = 100;
     else
       quality = 2 * (dB + 100);
-    return quality
+    return quality;
   },
 
   /** nbre de barre wifi selon % quality) **/
-  wifiBar(percent) {
-    return parseInt(((percent*5)/100).toFixed(0))
+  wifiBar (percent) {
+    return parseInt(((percent*5)/100).toFixed(0));
   },
 
-  ModelSearh(model) {
+  ModelSearh (model) {
     try {
-      const freebox = model.split(" ")
-      var version = freebox[1]
-      const rev = freebox[2]
+      const freebox = model.split(" ");
+      var version = freebox[1];
+      const rev = freebox[2];
       switch (version) {
         case "v9":
           version = "Ultra";
@@ -400,16 +399,16 @@ module.exports = NodeHelper.create({
           version = "Révolution";
           break;
         default:
-          console.warn("[Freebox] Freebox Version not found !")
-          console.warn("[Freebox] Please contact developer with this freebox model:", model)
-          break
+          console.warn("[Freebox] Freebox Version not found !");
+          console.warn("[Freebox] Please contact developer with this freebox model:", model);
+          break;
       }
-      let FreeboxName = `Freebox ${version} ${rev}`
-      return FreeboxName
+      let FreeboxName = `Freebox ${version} ${rev}`;
+      return FreeboxName;
     } catch (e) {
-      console.warn("[Freebox] Error Freebox model formating!")
-      console.warn("[Freebox] Please contact developer with this freebox model:", model)
-      return model
+      console.warn("[Freebox] Error Freebox model formating!");
+      console.warn("[Freebox] Please contact developer with this freebox model:", model);
+      return model;
     }
   }
 });
